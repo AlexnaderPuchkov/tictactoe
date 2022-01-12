a51e186cffae5820ff6b2fad93d78e2e65da8e5a
AlexanderPuchkov <zaneiyaaa@gmail.com>
2022-01-12 19:40:16 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './index.css';
 
-function App() {
+const App = () => {
+  const [turn, setTurn] = useState('x');
+  const [cells, setCells] = useState(Array(9).fill(''));
+  const [winner, setWinner] = useState();
+
+  const checkForWinner = (squares) => {
+    let combos = {
+      across: [
+              [0,1,2],
+              [3,4,5],
+              [6,7,8],
+      ],
+      down: [
+              [0,3,6],
+              [1,4,7],
+              [2,5,8],
+      ],
+      diagonal: [
+              [0,4,8],
+              [2,4,6],
+      ],
+    }
+
+    for (let combo in combos) {
+      combos[combo].forEach((pattern) => {
+        if (
+          squares[pattern[0]] === '' ||
+          squares[pattern[1]] === '' ||
+          squares[pattern[2]] === ''
+        ) {
+          // do nothing
+        }
+        else if (
+                squares[pattern[0]] === squares[pattern[1]] &&
+                squares[pattern[1]] === squares[pattern[2]])
+                 {
+                   setWinner(squares[pattern[0]]);
+              }
+      });
+    }
+
+  }
+
+  const handleClick = (num) => {
+    if (cells[num] !== '') {
+      alert('already clicked');
+      return;
+    }
+    let squares = [...cells];
+
+    if (turn === 'x') {
+      squares[num] = 'x';
+      setTurn ('o');
+    } else {
+      squares[num] = 'o';
+      setTurn('x');
+    }
+
+    checkForWinner(squares);
+    setCells(squares);
+
+    };
+
+    const handleRestart = () => {
+      setWinner(null);
+      setCells(Array(9).fill(''));
+
+    }
+  
+
+  const Cell = ({num}) => {
+    return <td onClick={() => handleClick(num)}>{cells[num]}</td>
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <table>
+        Turn: {turn}
+        <tbody>
+          <tr>
+            <Cell num={0} />
+            <Cell num={1} />
+            <Cell num={2} />
+          </tr>
+          <tr>
+            <Cell num={3} />
+            <Cell num={4} />
+            <Cell num={5} />
+          </tr>
+          <tr>
+            <Cell num={6} />
+            <Cell num={7} />
+            <Cell num={8} />
+          </tr>
+        </tbody>
+      </table>
+      {winner && (
+        <>
+      <p>{winner} is the winner!</p>
+      <button onClick={() => handleRestart()}>Play Again</button>
+      </>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
+
